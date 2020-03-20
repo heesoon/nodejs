d1371a8853d124ee27de88ac6a74965a4f9634fd
heesoon <chipmaker0304@gmail.com>
2020-03-20 06:22:16 +0000

    react redux test

---
+++ redux-test/src/store/modules/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+
+export default combineReducers({
+//	counter,
+   // other reducer can be added here
+	counter : counter,
+});
