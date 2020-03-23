175286436ed5a9909fd912b5a05a6352f2d5b6b9
heesoon <chipmaker0304@gmail.com>
2020-03-23 03:19:24 +0000

    branch rerarrange

--- redux-test/src/store/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-
-export default combineReducers({
-//	counter,
-   // other reducer can be added here
-	counter : counter,
-});
