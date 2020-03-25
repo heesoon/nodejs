4e30f6b8e02d97f1abbc279d9332f21ab7eed587
heesoon <chipmaker0304@gmail.com>
2020-03-25 03:50:59 +0000

    add redux-multicounter example

---
+++ redux-multicounter/README.md
@@ -0,0 +1,7 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Reference
+
+This example is referenced to https://velopert.com/3365
+The purpose of this project is only for personal study. So I just implement reference site code as same step.
+
---
+++ redux-multicounter/src/index.js
@@ -0,0 +1,20 @@
+// reference -> https://velopert.com/3352
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+//import App from './components/App';
+import './index.css';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
