35a2f6aff6cfea4d26872e96c8e41a761384d3df
heesoon <chipmaker0304@gmail.com>
2020-03-25 07:11:34 +0000

    modify for immutable

--- redux-counter-immutable/src/components/CounterList.js
+++ redux-counter-immutable/src/components/CounterList.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import Counter from './Counter';
 import PropTypes from 'prop-types';
+import { List } from 'immutable';
 
 import './CounterList.css';
 
 const CounterList = ({counters, onIncrement, onDecrement, onSetColor}) => {
 
-	const counterList = counters.map(
-		(counter, i) => (
-			<Counter
-				key={i}
-				index={i}
-				{...counter}
-				onIncrement={onIncrement}
-				onDecrement={onDecrement}
-				onSetColor={onSetColor}
-			/>
-		)
-	);
+    const counterList = counters.map(
+        (counter, i) => (
+            <Counter 
+                key={i}
+                index={i}
+                {...counter.toJS()}
+                onIncrement={onIncrement}
+                onDecrement={onDecrement}
+                onSetColor={onSetColor}
+            />
+        )
+    );
 
-	return(
-		<div className="CounterList">
-			{counterList}
-		</div>
-	);
+    return (
+        <div className="CounterList">
+            {counterList}
+        </div>
+    );
 };
 
 CounterList.propTypes = {
-    counters: PropTypes.arrayOf(PropTypes.shape({
-        color: PropTypes.string,
-        number: PropTypes.number
-    })),
+    counters: PropTypes.instanceOf(List),
     onIncrement: PropTypes.func,
     onDecrement: PropTypes.func,
     onSetColor: PropTypes.func
